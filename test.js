0d3aae352da31e30b0b2a9789137e1763c1afbf2
Matthew <unicomatthew@gmail.com>
2023-02-11 00:22:45 -0800

    hellp

---
+++ test.js
@@ -0,0 +1,42 @@
+// const data = ['car', 'car', 'truck', 'truck', 'bike', 'walk', 'car', 'van', 'bike', 'walk', 'car', 'van', 'car', 'truck', 'pogostick'];
+
+
+// data.forEach(element => {
+//     console.log(element)
+    
+// });
+
+// const DATABOY = data.forEach(element => console.log(`my preferred method of transportation is ${element} cuz i love it`))
+
+
+// const inventors = [
+//     { first: 'Albert', last: 'Einstein', year: 1879, passed: 1955 },
+//     { first: 'Isaac', last: 'Newton', year: 1643, passed: 1727 },
+//     { first: 'Galileo', last: 'Galilei', year: 1564, passed: 1642 },
+//     { first: 'Marie', last: 'Curie', year: 1867, passed: 1934 },
+//     { first: 'Johannes', last: 'Kepler', year: 1571, passed: 1630 },
+//     { first: 'Nicolaus', last: 'Copernicus', year: 1473, passed: 1543 },
+//     { first: 'Max', last: 'Planck', year: 1858, passed: 1947 },
+//     { first: 'Katherine', last: 'Blodgett', year: 1898, passed: 1979 },
+//     { first: 'Ada', last: 'Lovelace', year: 1815, passed: 1852 },
+//     { first: 'Sarah E.', last: 'Goode', year: 1855, passed: 1905 },
+//     { first: 'Lise', last: 'Meitner', year: 1878, passed: 1968 },
+//     { first: 'Hanna', last: 'Hammarström', year: 1829, passed: 1909 }
+//   ];
+
+//   const MAPARRAYBOY = inventors.map(data => {
+
+
+
+
+//     console.log(data.first + ` ` + data.last )
+
+// })
+// const OLDMAN = inventors.filter(data => data.year > 1970);
+// console.log(OLDMAN)
+const button1 = document.querySelector('.button')
+
+button1.addEventListener('click',function(){
+
+    location.assign('savedhtml')
+})
